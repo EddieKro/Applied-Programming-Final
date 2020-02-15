b61acfe833d2533f189a3b2d5503446858ebdd2d
Tomato <dev.artem9@gmail.com>
2020-02-15 23:42:46 +0200

    test some lambda telegram respose

--- telegaLambda/lambda.py
+++ telegaLambda/lambda.py
@@ -1,6 +1,20 @@
 import json
+import os
+from telegram.ext import Dispatcher
+from telegram.ext import MessageHandler, Filters
+from telegram import Update, Bot
 
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+bot = Bot(token=os.getenv('TOKEN'))
+dispatcher = Dispatcher(bot, None, use_context=True)
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
 
 def handler(event, context):
-    
+    dispatcher.process_update(
+        Update.de_json(json.loads(event['body']), bot)
+    )
     return {"statusCode": 200}

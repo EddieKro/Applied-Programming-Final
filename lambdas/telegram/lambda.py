e87ef1b94a1a0ee66ec5b7cf86101db92ec7aef1
Oleksiy <oleksiy@syncwords.com>
2020-04-05 17:44:32 +0300

    Changed bytes to list in post request.

--- lambdas/telegram/lambda.py
+++ lambdas/telegram/lambda.py
@@ -6,7 +6,7 @@ from telegram.ext import Dispatcher
 from telegram.ext import MessageHandler, Filters
 from telegram import Update, Bot, Document
 import boto3
-
+import numpy as np
 ml_url = os.getenv('ML_URL')
 
 def echo(update, context):
@@ -14,10 +14,10 @@ def echo(update, context):
     print(context)
     print(update.message.document.file_id)
     doc = Document(file_id=update.message.document.file_id, bot=bot)
-    image_b = bytes(doc.get_file().download_as_bytearray())
+    image_b = np.frombuffer(doc.get_file().download_as_bytearray(),dtype=np.uint8).tolist()
     chat_id = update.effective_chat.id
     request = {'image': image_b, 'chat_id': chat_id}
-    r = requests.post( url = ml_url, data = request)
+    r = requests.post( url = ml_url, json = request)
     # response = client.put_object(Body=ba, Bucket=os.getenv('DICOM_Bucket'), Key=f'{update._effective_user.id}/{update.message.document.file_name}')
     # context.bot.send_message(chat_id=update.effective_chat.id, text='cool, u have sent me a dcm')
 

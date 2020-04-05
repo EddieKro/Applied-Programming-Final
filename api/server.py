0493bb1b74b359cc0fd0e2d35855097636cc8217
Oleksiy <oleksiy@syncwords.com>
2020-04-05 17:50:29 +0300

    Changed dependency

--- api/server.py
+++ api/server.py
@@ -3,7 +3,7 @@ import flask
 from flask import Flask, request
 from xray_model.xray_covid_nn import NN_model
 import os
-
+import requests
 app = Flask(__name__)
 model = NN_model()
 
@@ -17,7 +17,7 @@ def image_covid_prediction():
 	inputs = flask.request.get_json(force=True)
 	preds = model.predict(inputs["image"])
 	message = f"You have a {preds[1]*100:.2f}% chance of {'not ' if preds[0]==0 else ''}having COVID-19."
-	request.post(os.getenv('Message_resp_url'), json={"message": message, "chat_id": inputs["chat_id"]})
+	requests.post(os.getenv('Message_resp_url'), json={"message": message, "chat_id": inputs["chat_id"]})
 
 if __name__ == '__main__':
 	app.run('0.0.0.0', 8181)

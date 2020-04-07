1735c2d01ed178f27a2e3d3598276605a3d1ea6a
Alexandr Kroshyn <adkrobel1@gmail.com>
2020-04-07 10:50:56 +0300

    covid symptoms added to API

--- api/server.py
+++ api/server.py
@@ -2,14 +2,31 @@ import numpy as np
 import flask
 from flask import Flask, request
 from xray_model.xray_covid_nn import NN_model
+from symptoms_model.symptoms_covid import Symptoms
 import os
 import requests
 app = Flask(__name__)
-model = NN_model()
+nn_model = NN_model()
+symptoms_model = Symptoms.load_model()
 
 @app.route('/app/health')
 def health():
 	return {'status': 'ok'}
+
+
+@app.route('/symptoms/predict', methods=['POST'])
+def syms_covid_prediction():
+	inputs = flask.request.get_json(force=True)
+	preds = symptoms_model.predict(inputs)
+    
+    if preds == 0:
+        msg = f" probably don't have COVID-19 right now. Be careful, stay home, everything will be fine!"
+    else:
+        msg = f" should contact your doctor to get yourself tested. Please, don't panic and follow official instructions"
+    
+	message = f"Looks like your symptoms are{'n`t' if preds==0 else ''} severe. You" + msg
+	requests.post(os.getenv('Message_resp_url'), json={"message": message, "chat_id": inputs["chat_id"]})
+
 
 # takes image as url.
 @app.route('/xray/predict', methods=['POST'])

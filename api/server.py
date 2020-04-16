02cec00195afdb209898a74cfa1eba21cea8d569
Alexandr Kroshyn <adkrobel1@gmail.com>
2020-04-16 23:05:57 +0300

    fixes #4

--- api/server.py
+++ api/server.py
@@ -2,10 +2,14 @@ import numpy as np
 import flask
 from flask import Flask, request
 from symptoms_model.symptoms_covid import Symptoms
+from xray_model.xray_covid_nn import NN_model
 import os
 import requests
+
 app = Flask(__name__)
 symptoms_model = Symptoms().load_model()
+nn_model = NN_model()
+
 
 @app.route('/app/health')
 def health():
@@ -16,7 +20,6 @@ def health():
 def syms_covid_prediction():
     data = flask.request.get_json(force=True)
     inputs = np.array(data['symptoms']).reshape(1,-1)
-    
     preds = symptoms_model.predict(inputs)[0]
     if preds == 0:
         msg = f" probably don't have COVID-19 right now. Be careful, stay home, everything will be fine!"
@@ -24,7 +27,10 @@ def syms_covid_prediction():
         msg = f" should contact your doctor to get yourself tested. Please, don't panic and follow official instructions"
     
     message = f"Looks like your symptoms are{'n`t' if preds==0 else ''} severe. You" + msg
-    requests.post(os.getenv('Message_resp_url'), json={"message": message, "chat_id": data["chat_id"], "pred":preds})
-
+    data = {"message": message, "chat_id": data["chat_id"], "pred":int(preds)}
+    requests.post(os.getenv('Message_resp_url'), json=flask.jsonify(data))
+    return
+    
+    
 if __name__ == '__main__':
-    app.run('0.0.0.0', 8181)
+    app.run('0.0.0.0', 8181,debug=True)

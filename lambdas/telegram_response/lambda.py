1d84dcb108331532fca6a43ce09c61e91c5306e5
Alexandr Kroshyn <adkrobel1@gmail.com>
2020-04-16 21:12:42 +0300

    added pred to json flask output

--- lambdas/telegram_response/lambda.py
+++ lambdas/telegram_response/lambda.py
@@ -11,5 +11,7 @@ bot = Bot(token=os.getenv('TOKEN'))
 
 def handler(event, context):
     body = event['body']
+	
+
     bot.send_message(chat_id=body['chat_id'], text=body['text'])
     return {"statusCode": 200}
